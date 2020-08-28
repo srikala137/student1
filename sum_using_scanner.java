19382399e4863af309a4d26460ca8a9947429823
srikala137 <69665025+srikala137@users.noreply.github.com>
2020-08-28 20:39:25 +0530

    Add files via upload

---
+++ sum_using_scanner.java
@@ -0,0 +1,18 @@
+package pack_1;
+import java.util.*;
+import java.util.Scanner;
+public class sum_using_scanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter  the value of a");
+        int a=obj.nextInt();
+        System.out.println("enter the value of b");
+        int b=obj.nextInt();
+        int c=a+b;
+        System.out.println("the sum of a and b is c "+c);
+        
+	}
+
+}
