a9605445a29effcfc91cd406f828f54eb06d3a7d
srikala137 <69665025+srikala137@users.noreply.github.com>
2020-08-28 20:29:49 +0530

    Add files via upload

---
+++ single.java
@@ -0,0 +1,13 @@
+package pack_1;
+
+public class single {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("the sum of a and b is" +c);
+	}
+
+}
