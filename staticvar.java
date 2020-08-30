bb1afbdeee25a4c1bcfae75afa0793b68775729a
srikala137 <69665025+srikala137@users.noreply.github.com>
2020-08-30 22:35:24 +0530

    Add files via upload

---
+++ staticvar.java
@@ -0,0 +1,17 @@
+package pack_1;
+
+public class staticvar {
+	static int x =10;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		   staticvar t1 = new staticvar();
+		   t1.x=50;
+		   System.out.println(t1.x);  
+		   
+		   staticvar t2 = new staticvar();
+		   System.out.println(t2.x); 
+	}
+
+}
