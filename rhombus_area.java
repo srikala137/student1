baa53170418a7eaf73e0eab128f44e28bbeca7a6
srikala137 <69665025+srikala137@users.noreply.github.com>
2020-08-31 21:53:59 +0530

    Add files via upload

---
+++ rhombus_area.java
@@ -0,0 +1,15 @@
+package pack_1;
+import java.util.*;
+public class rhombus_area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner obj= new Scanner(System .in);
+      System .out.println("the diagonlas values ");
+      
+      int d1=obj.nextInt();
+      int d2=obj.nextInt();
+      double area=(1.0/2.0)*d1*d2;
+      System.out.println("the area of rhombus"+area);
+	}
+}
