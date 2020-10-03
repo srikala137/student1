96542bf450682a6db14560ad243e231a367d9189
srikala137 <69665025+srikala137@users.noreply.github.com>
2020-10-03 19:51:08 +0530

    Add files via upload

---
+++ Pattern22.java
@@ -0,0 +1,16 @@
+
+public class Pattern22 {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=n-1;j<=i;j--)
+				System.out.print(" ");
+			for(int k=1;k<=i;k++)
+				System.out.print("* ");
+	   System.out.println("");
+		}
+		
+
+	}}
