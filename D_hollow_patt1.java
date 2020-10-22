29fd6f6747370b7abff795d3f4b4e0df3cfda4cf
srikala137 <69665025+srikala137@users.noreply.github.com>
2020-10-22 17:55:50 +0530

    Add files via upload

---
+++ D_hollow_patt1.java
@@ -0,0 +1,29 @@
+package pack_3;
+
+public class D_hollow_patt1 {
+
+	public static void main(String[] args) {
+		int n=9;
+		int x=n/2+1;
+		for(int i=1;i<=n;i++)
+
+		{
+			for(int j=1;j<=n;j++)
+			{
+				if(j==x || j==n-x+1)
+					System.out.print(x);
+				else
+					System.out.print(" ");
+			}
+			if(i<=n/2)
+				x=x-1;
+			else
+				x=x+1;
+			System.out.println();
+		}
+		
+		
+
+	}
+
+}
